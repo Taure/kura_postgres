3710165fc4a171d6c8c299cc2bd607bebff2dd9d
Daniel Widgren <daniel.widgren@gmail.com>
2026-05-10 08:48:35 +0200

    feat: add ensure_database/probe_pool to kura_driver_pgo (#1)

--- src/kura_driver_pgo.erl
+++ src/kura_driver_pgo.erl
@@ -22,7 +22,9 @@ so non-pgo drivers can substitute their own transaction conventions.
 -export([
     query/5,
     query_on/4,
-    transaction/4
+    transaction/4,
+    ensure_database/1,
+    probe_pool/1
 ]).
 
 -define(DEFAULT_DECODE_OPTS, [return_rows_as_maps, column_name_as_atom]).
@@ -50,3 +52,97 @@ query_on(Conn, SQL, Params, Opts) ->
 transaction(_PoolMod, Pool, Fun, Opts) ->
     PgoOpts = maps:with([pool_options], Opts),
     pgo:transaction(Pool, Fun, PgoOpts).
+
+-spec ensure_database(map()) -> ok | {error, term()}.
+ensure_database(Config) ->
+    case maps:find(database, Config) of
+        error ->
+            ok;
+        {ok, DbName} ->
+            do_ensure_database(Config, binary_to_list(DbName))
+    end.
+
+-spec probe_pool(kura_pool:name()) -> ok | {error, term()}.
+probe_pool(Pool) ->
+    try pgo:query(~"SELECT 1", [], #{pool => Pool}) of
+        #{rows := _} -> ok;
+        {error, Reason} -> {error, Reason}
+    catch
+        Class:CatchReason -> {error, {Class, CatchReason}}
+    end.
+
+%%----------------------------------------------------------------------
+%% Internal: bootstrap the maintenance DB to CREATE DATABASE if missing.
+%%----------------------------------------------------------------------
+
+do_ensure_database(Config, Database) ->
+    TmpPool = kura_migrator_tmp_pool,
+    TmpConfig = apply_pool_extras(base_pool_config(Config, Database)),
+    case try_connect(TmpPool, TmpConfig) of
+        ok ->
+            stop_tmp_pool(TmpPool);
+        {error, _} ->
+            stop_tmp_pool(TmpPool),
+            create_database(Config, Database)
+    end.
+
+base_pool_config(Config, Database) ->
+    #{
+        host => binary_to_list(maps:get(hostname, Config, ~"localhost")),
+        port => maps:get(port, Config, 5432),
+        database => Database,
+        user => binary_to_list(maps:get(username, Config, ~"postgres")),
+        password => binary_to_list(maps:get(password, Config, <<>>)),
+        pool_size => 1,
+        decode_opts => ?DEFAULT_DECODE_OPTS
+    }.
+
+apply_pool_extras(Base) ->
+    WithSocket =
+        case application:get_env(kura, socket_options, []) of
+            Opts when is_list(Opts), Opts =/= [] -> Base#{socket_options => Opts};
+            _ -> Base
+        end,
+    WithSSL =
+        case application:get_env(kura, ssl, false) of
+            true -> WithSocket#{ssl => true};
+            _ -> WithSocket
+        end,
+    case application:get_env(kura, ssl_options, []) of
+        SSLOpts when is_list(SSLOpts), SSLOpts =/= [] ->
+            WithSSL#{ssl_options => SSLOpts};
+        _ ->
+            WithSSL
+    end.
+
+try_connect(TmpPool, TmpConfig) ->
+    case pgo_sup:start_child(TmpPool, TmpConfig) of
+        {ok, _} ->
+            case pgo:query(~"SELECT 1", [], #{pool => TmpPool}) of
+                #{rows := _} -> ok;
+                {error, Reason} -> {error, Reason}
+            end;
+        {error, {already_started, _}} ->
+            ok;
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+create_database(Config, Database) ->
+    TmpPool = kura_migrator_create_pool,
+    TmpConfig = apply_pool_extras(base_pool_config(Config, "postgres")),
+    case pgo_sup:start_child(TmpPool, TmpConfig) of
+        {ok, _} -> ok;
+        {error, {already_started, _}} -> ok
+    end,
+    DbBin = list_to_binary(Database),
+    QuotedDb = iolist_to_binary([<<"\"">>, DbBin, <<"\"">>]),
+    SQL = iolist_to_binary([~"CREATE DATABASE ", QuotedDb]),
+    _ = pgo:query(SQL, [], #{pool => TmpPool}),
+    logger:info(#{msg => ~"kura: created database", database => Database}),
+    stop_tmp_pool(TmpPool).
+
+stop_tmp_pool(TmpPool) ->
+    _ = supervisor:terminate_child(pgo_sup, TmpPool),
+    _ = supervisor:delete_child(pgo_sup, TmpPool),
+    ok.
